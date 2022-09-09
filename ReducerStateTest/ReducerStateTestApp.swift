ba238912b9c5913a024dd3601fb2781f36c19bbb
Rhys Morgan <rhysm@me.com>
2022-09-09 11:37:45 +0100

    Initial Commit

---
+++ ReducerStateTest/ReducerStateTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReducerStateTestApp.swift
+//  ReducerStateTest
+//
+//  Created by Rhys Morgan on 09/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct ReducerStateTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
